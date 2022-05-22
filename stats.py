6bf106486242cc24ebed0c02d09cc35981799cd8
Krystal <72570954+Krysstals@users.noreply.github.com>
2022-05-22 16:44:59 +0200

    Add files via upload

--- stats.py
+++ stats.py
@@ -1,31 +1,42 @@
 import datetime
 import requests
 import json
+
 from os import system
 system("title Stats Rinaorc by Krystal#6960")
 system("mode 78, 10")
+
+
 def clear():
     system("cls")
+
+
 with open("settings.json", "r") as f:
     config = json.load(f)
 header = {"API-Key": config["APIKey"]}
 username = input('\n\n\n\n                    [>] Player Name: ')
-r = requests.get(url=f"https://api.rinaorc.com/player/{username}", headers=header)
+r = requests.get(
+    url=f"https://api.rinaorc.com/player/{username}", headers=header)
 data = r.json()
 
 a = requests.get(url=f"https://api.rinaorc.com/stats/players", headers=header)
 serverInfo = a.json()
+input(serverInfo)
+
 
 def menu():
     clear()
     system("mode 78, 10")
-    choice = input("\n                                Choose Info:\n\n        [1] User Info        [2] BedWars Info        [3] Server Info\n\n        [4] Clan Info\n\n                                 Choice: ")
+    choice = input(
+        "\n                                Choose Info:\n\n        [1] User Info        [2] BedWars Info        [3] Server Info\n\n        [4] Clan Info\n\n                                 Choice: ")
     clear()
     if choice == "1":
         system("mode 78, 25")
         playerData = data["player"]
-        if playerData["isOnline"] == True: online="Online"
-        else: online="Offline"
+        if playerData["isOnline"] == True:
+            online = "Online"
+        else:
+            online = "Offline"
         print(f'''
             : Player Information :
             
@@ -121,36 +132,39 @@ def menu():
     if choice == "3":
         system("mode 50, 35")
         print(f'''
-            : Server Info :
-        Active Player: {serverInfo["activePlayers"]}
-        Online Players: {serverInfo["onlinePlayers"]}
+                : Server Info :
+            Active Player: {serverInfo["activePlayers"]}
+            Online Players: {serverInfo["onlinePlayers"]}
 
-            :     BAN     :
-        Total Bans: {serverInfo["punishments"]["bans"]["totalBans"]}
-        Monthly Bans: {serverInfo["punishments"]["bans"]["monthlyBans"]}
-        Weekly Bans: {serverInfo["punishments"]["bans"]["weeklyBans"]}
-        
-            :     MUTE    :
-        Total Mutes: {serverInfo["punishments"]["mutes"]["totalMutes"]}
-        Monthly Mutes: {serverInfo["punishments"]["mutes"]["monthlyMutes"]}
-        Weekly Mutes: {serverInfo["punishments"]["mutes"]["weeklyMutes"]}
-        
-           :PLAYERS IN GAME:
-        BedWars: {serverInfo["games"]["bedwars"]["players"]}
-        Caveaux: {serverInfo["games"]["caveaux"]["players"]}
-        Free Build: {serverInfo["games"]["freebuild"]["players"]}
-        Golem Rush: {serverInfo["games"]["golemrush"]["players"]}
-        Gun Wars: {serverInfo["games"]["gunwars"]["players"]}
-        Lobby: {serverInfo["games"]["lobbyMain"]["players"]}
-        Parkour: {serverInfo["games"]["parkour"]["players"]}
-        Pixel Perfect: {serverInfo["games"]["pixelperfect"]["players"]}
-        Practice: {serverInfo["games"]["practice"]["players"]+serverInfo["games"]["pvpbox"]["players"]+serverInfo["games"]["pvpenchants"]["players"]}
-        Sheep Wars: {serverInfo["games"]["sheepwars"]["players"]}
-        Sky Wars: {serverInfo["games"]["skywars"]["players"]}
-        Smash: {serverInfo["games"]["smash"]["players"]}
-        The Purge: {serverInfo["games"]["thepurge"]["players"]}
-        UHC: {serverInfo["games"]["uhc"]["players"]}
-        Warfare: {serverInfo["games"]["warfare"]["players"]}''')
+                :     BAN     :
+            Total Bans: {serverInfo["punishments"]["bans"]["totalBans"]}
+            Monthly Bans: {serverInfo["punishments"]["bans"]["monthlyBans"]}
+            Weekly Bans: {serverInfo["punishments"]["bans"]["weeklyBans"]}
+            
+                :     MUTE    :
+            Total Mutes: {serverInfo["punishments"]["mutes"]["totalMutes"]}
+            Monthly Mutes: {serverInfo["punishments"]["mutes"]["monthlyMutes"]}
+            Weekly Mutes: {serverInfo["punishments"]["mutes"]["weeklyMutes"]}
+            
+            :PLAYERS IN GAME:
+
+            Login: {serverInfo["games"]["login"]["players"]}
+            
+            BedWars: {serverInfo["games"]["bedwars"]["players"]}
+            Caveaux: {serverInfo["games"]["caveaux"]["players"]}
+            Free Build: {serverInfo["games"]["freebuild"]["players"]}
+            Golem Rush: {serverInfo["games"]["golemrush"]["players"]}
+            Gun Wars: {serverInfo["games"]["gunwars"]["players"]}
+            Lobby: {serverInfo["games"]["lobbyMain"]["players"]}
+            Parkour: {serverInfo["games"]["parkour"]["players"]}
+            Pixel Perfect: {serverInfo["games"]["pixelperfect"]["players"]}
+            Practice: {serverInfo["games"]["practice"]["players"]+serverInfo["games"]["pvpbox"]["players"]+serverInfo["games"]["pvpenchants"]["players"]}
+            r/Place: {serverInfo["games"]["rplace"]["players"]}
+            Sheep Wars: {serverInfo["games"]["sheepwars"]["players"]}
+            Smash: {serverInfo["games"]["smash"]["players"]}
+            The Purge: {serverInfo["games"]["thepurge"]["players"]}
+            UHC: {serverInfo["games"]["uhc"]["players"]}
+            Warfare: {serverInfo["games"]["warfare"]["players"]}''')
         input()
 
     if choice == "4":
@@ -175,10 +189,12 @@ def menu():
         Total XP: {clanData["totalExperience"]}
         """)
         input()
-    
+
+
 while True:
     system("mode 78, 10")
-    try: menu()
-    except: 
+    try:
+        menu()
+    except:
         system("mode 78, 10")
         input(' An error has occurred. There is many reasons for that:\n 1) Rinaorc\'s API is down.\n 2) The user provided has never logged on Rinaorc.\n 3) Your API Key is not valid, change it in the settings.')
